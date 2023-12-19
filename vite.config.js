15df233ea10d49c3a9f6f9ead5b16038b8c945c9
izleenfaqihah <izleenfaqihah@gmail.com>
2023-12-19 14:32:05 +0800

    add code

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/animation3d/",
   plugins: [react()],
+  assetsInclude: ['**/*.glb'],
 })
